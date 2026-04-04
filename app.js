94efeac07c08953d789c6a745760bbb3d3bb2182
Odilbekov O`lmasbek <oolmasbek754@gmail.com>
2026-04-04 14:35:15 +0500

    Add driving eligibility check based on age and country

---
+++ app.js
@@ -0,0 +1,20 @@
+let age = 16;
+let country = "USA";
+let text = "You can Not drive!";
+
+
+if (country == "USA") {
+  if (age >= 16) {
+    text = "You can drive!";
+  }
+}
+
+
+let age = 16;
+let country = "USA";
+let text = "You can Not drive!";
+
+
+if (country == "USA" && age >= 16) {
+  text = "You can drive!";
+}
